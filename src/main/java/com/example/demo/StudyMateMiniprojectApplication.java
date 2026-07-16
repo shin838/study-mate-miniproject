50cdb5a10b2f2172ae1d4f6e97b3724862b5b558
shin838 <21stseptemberr@gmail.com>
2026-07-16 09:51:14 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/demo/StudyMateMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMateMiniprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMateMiniprojectApplication.class, args);
+	}
+
+}
