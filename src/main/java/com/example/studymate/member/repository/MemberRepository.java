e179276a5246d2c02923a6c84bc054d45442ff99
tost7 <tost7@MAIN>
2026-07-20 13:37:45 +0900

    member,repo

---
+++ src/main/java/com/example/studymate/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.studymate.member.repository;
+
+import com.example.studymate.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
