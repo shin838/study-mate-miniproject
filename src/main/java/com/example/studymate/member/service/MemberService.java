1b9c2826d53c6f4cf427a6e109f7faa847e6fadd
tost7 <tost7@MAIN>
2026-07-21 17:02:22 +0900

    jwt, spring security 구현 , 포스트맨으로 회원가입, 토큰 발급, 관리자 권한(멤버 전체 리스트 확인) # Conflicts: #	src/main/java/com/example/studymate/member/controller/AdminMemberController.java #	src/main/java/com/example/studymate/member/service/MemberService.java #	src/main/java/com/example/studymate/member/service/MemberServiceImpl.java

---
+++ src/main/java/com/example/studymate/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.example.studymate.member.service;
+
+import java.util.List;
+
+import com.example.studymate.member.dto.MemberRequestDto;
+import com.example.studymate.member.dto.MemberResponseDto;
+import com.example.studymate.member.entity.Member;
+
+public interface MemberService {
+    MemberResponseDto registerMember(MemberRequestDto memberDto);
+    
+    // 관리자용: 전체 회원 목록 조회
+    List<Member> getAllMembers();
+    
+    // 관리자용: 회원 삭제
+    MemberResponseDto deleteMember(Integer memberId);
+}
